529f728df8d66a82edfa2978aa4ca90e40f5abe7
Ben Forest <ben@ben-forest.com>
2019-07-30 10:56:06 -0400

    add branch checkout to startup script

--- startup.bat
+++ startup.bat
@@ -1,3 +1,9 @@
 @echo off
 :: this script goes in the actual startup folder to launch the main script
+E:
+cd \scripts\Ariadne-Observance-Startup
+git checkout master
+echo script branch:
+git rev-parse --abbrev-ref HEAD
+
 E:\scripts\Ariadne-Observance-Startup/test.bat
